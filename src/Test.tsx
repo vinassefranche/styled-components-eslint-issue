9129f1e6ce427bb8e7b5134601890a5e8e875c56
vinassefranche <vincent.francois@inato.com>
2021-05-03 17:54:17 +0200

    Reproduce issue

---
+++ src/Test.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+export const WorkingLint: React.FunctionComponent<{prop?: string}> = ({prop}) => {
+  if(!prop) {
+    return null;
+  }
+  const [] = useState(false);
+  return (
+    <div> Lint on hook is working </div>
+  );
+};
+
+export const NotWorkingLint = styled(({prop}:{prop?: string}) => {
+  if(!prop) {
+    return null;
+  }
+  const [] = useState(false);
+  return (
+    <div> Lint on hook is working </div>
+  );
+})``;
+
